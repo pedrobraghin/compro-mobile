4b22dd88bcfa3d6e58a7b455dff24dad25ddfdc0
Victor <linkgamesplays@gmail.com>
2023-12-26 11:36:40 -0300

    chore(tailwind): create variables for base colors & themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,31 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: {
+          text: '#000',
+          background: '#fff',
+          tint: '#2f95dc',
+          tabIconDefault: '#ccc',
+          tabIconSelected: '#2f95dc',
+        },
+        dark: {
+          text: '#fff',
+          background: '#000',
+          tint: '#fff',
+          tabIconDefault: '#ccc',
+          tabIconSelected: '#fff',
+        },
+        general: {
+          lightRed: '#C8353C',
+          darkRed: '#8D060C',
+          yellow: '#FFDA00',
+          textBlur: '#F4F4F4',
+          textColor: '#ffffff'
+        }
+      }
+    },
   },
   plugins: [],
 };
