7aeca970ca5ba097a21eac2e42fa124c3e3d96e0
Victor <linkgamesplays@gmail.com>
2023-12-26 11:36:08 -0300

    feat(constants): create constants for tailwind styled components

---
+++ src/constants/StyledComponents.ts
@@ -0,0 +1,9 @@
+import { View, Text, Pressable, TextInput, SafeAreaView, TouchableHighlight } from "react-native";
+import { styled } from 'nativewind';
+
+export const StyledView = styled(View);
+export const StyledText = styled(Text);
+export const StyledPressable = styled(Pressable)
+export const StyledInput = styled(TextInput)
+export const StyledSafeAreaView = styled(SafeAreaView)
+export const StyledTouchableHighlight = styled(TouchableHighlight)
