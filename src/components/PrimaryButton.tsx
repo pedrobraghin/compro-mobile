e3c16f3553ebc25c829f97f9c84f671ea519c916
Victor <linkgamesplays@gmail.com>
2023-12-26 11:35:31 -0300

    feat(components): edit primarybutton component for tailwindcss

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -1,24 +1,19 @@
 import { TouchableHighlight, Text } from 'react-native';
 import Colors from '../constants/Colors';
+import { StyledTouchableHighlight } from '../constants/StyledComponents';
 
 interface PrimaryButtoProps {
-  onPress?: () => void;
-  text: string;
+	onPress?: () => void;
+	text: string;
 }
 
 export function PrimaryButton({ onPress, text }: PrimaryButtoProps) {
-  return (
-    <TouchableHighlight onPress={onPress} style={{
-      backgroundColor: Colors.general.darkRed,
-      borderRadius: 32,
-      width: 'auto',
-      paddingHorizontal: 16,
-      paddingVertical: 12,
-      alignItems: 'center',
-    }}>
-      <Text style={{ color: Colors.general.textColor }}>
-        {text}
-      </Text>
-    </TouchableHighlight>
-  )
+	return (
+		<StyledTouchableHighlight
+			onPress={onPress}
+			className="bg-general-darkRed rounded-3xl px-4 py-3 w-56 items-center"
+		>
+			<Text style={{ color: Colors.general.textColor }}>{text}</Text>
+		</StyledTouchableHighlight>
+	);
 }
